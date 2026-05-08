1d7e588581e299b9c5e336436042b25e372ea4db
Harshita <harshitapanwar0910@gmail.com>
2026-05-08 10:02:01 +0530

    destructuring

---
+++ Medium/04_object_d.js
@@ -0,0 +1,22 @@
+//Object Destructuring
+
+const school = {
+    name: "IBS Global Academy",
+    address: "Ujjain",
+    principal: "Mr. Sharma",
+    estd: 2005
+}
+
+//destructuring
+const { name } = school;
+console.log(name);
+
+//Similarly, we can also destruct all in one line
+const { principal, address } = school;
+console.log(principal);
+console.log(address);
+
+//Also, we can rename these keys into new variables
+const { estd: established } = school;
+console.log(established);
+

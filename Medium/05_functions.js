5a98dab33da66c1ac385b1d31863cd7ca73ad915
Harshita <harshitapanwar0910@gmail.com>
2026-05-11 10:40:19 +0530

    passing different values as function parameters

--- Medium/05_functions.js
+++ Medium/05_functions.js
@@ -24,3 +24,32 @@ function loginUserMessage(username = "unknownUser") {
 
 console.log(loginUserMessage("Harshita"));
 console.log(loginUserMessage());
+
+
+//When number of coming parameters are not known
+function calculateCartPrice(...num1) {
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 400, 600, 800));
+
+
+//When we do not know the parameters but we know object will be there
+//Passing object to function
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`);
+}
+
+handleObject({
+    name: "Harshita",
+    price: 200
+})
+
+//Passing array to function
+const myArr = [200, 400, 600, 800]
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myArr));

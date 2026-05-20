fe1a41ea923daa135a498eead7ff1dfccd74b831
Harshita <harshitapanwar0910@gmail.com>
2026-05-20 10:04:07 +0530

    Control Flow one added

---
+++ C_ControlFlow/01_one.js
@@ -0,0 +1,25 @@
+//if
+let one = 1;
+
+if (one <= 1) {
+    console.log("one is less then or equal to 1");
+}
+else {
+    console.log("one is greater than 1");
+}
+
+
+//if else
+if (one < 1) {
+    console.log("one is less then or equal to 1");
+}
+else if (one === 1) {
+    console.log("one is equal to 1");
+}
+else {
+    console.log("one is greater than 1");
+}
+
+
+//another way to write if 
+if (one <= 1) console.log("Executed1"), console.log("Executed2");
